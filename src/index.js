211724a33f5cfe7eb32ce50a78c7431fcfbde1db
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-12 16:32:35 +0100

    git add a modal handle function

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
-import getMovies from '../modules/movies.fetch';
-import moviesDisplay from '../modules/moviesDisplay';
+import getMovies from '../modules/movies.fetch.js';
+import moviesDisplay from '../modules/moviesDisplay.js';
+import modalHandle from '../modules/modalMovieDetails.js';
 
 getMovies().then((movieList) => {
   moviesDisplay(movieList);
+  modalHandle(movieList);
 });
