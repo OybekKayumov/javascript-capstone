246006db787aa3547dfeafcffd9947463eeafff2
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-12 17:02:53 +0100

    rename a handle modal function

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import './style.css';
 import getMovies from '../modules/movies.fetch.js';
 import moviesDisplay from '../modules/moviesDisplay.js';
-import modalHandle from '../modules/modalMovieDetails.js';
+import handleModal from '../modules/modalMovieDetails.js';
 
 getMovies().then((movieList) => {
   moviesDisplay(movieList);
-  modalHandle(movieList);
+  handleModal(movieList);
 });
