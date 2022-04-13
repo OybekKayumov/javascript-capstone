bcc76831f18eed9369a92d21c0bc1413754f5797
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-13 06:40:13 +0500

    add display reservation popup

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import './style.css';
 import getMovies from '../modules/movies.fetch';
 import moviesDisplay from '../modules/moviesDisplay';
+import reserveHandle from '../modules/reserveModal';
 
 getMovies().then((movieList) => {
   moviesDisplay(movieList);
+  reserveHandle(movieList);
 });
