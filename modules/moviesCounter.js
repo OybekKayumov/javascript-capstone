b519e4ae6367fcbda538d81a204aacb0b3142806
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-13 14:29:49 +0500

    add movies counter on home and update movieDisplay

---
+++ modules/moviesCounter.js
@@ -0,0 +1,27 @@
+const counter = (items) => {
+  let itemCount = 0;
+
+  items.forEach(() => {
+    itemCount += 1;
+  });
+  return itemCount;
+};
+
+const countDisplay = (count) => {
+  // const navList = document.querySelector('.nav-list');
+  // const li = document.createElement('li');
+  // console.log(li);
+
+  // li.innerHTML = `<li class="nav-item">(${count}) MOVIES</li>`;
+  // navList.appendChild('li');
+
+  const navList1 = document.querySelector('#count-movies');
+  navList1.innerHTML = `<li class="nav-item">(${count}) MOVIES1</li>`;
+};
+
+const countItems = (movies) => {
+  const number = counter(movies);
+  countDisplay(number);
+};
+
+export { countDisplay, countItems, counter };
