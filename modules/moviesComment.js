8e7ca43a5633ef426fd03ea80c56c1400823e002
Fahd Rahali <rahali.fahd@gmail.com>
2022-04-14 16:09:45 +0100

    add test for comment counter

--- modules/moviesComment.js
+++ modules/moviesComment.js
@@ -3,7 +3,7 @@ const baseURL = 'https://us-central1-involvement-api.cloudfunctions.net/capstone
 const appID = 'Ngyhlqhta3I7behhBDEq';
 
 const getComment = async (id) => {
-  const resolve = await window.fetch(`${baseURL}/${appID}/comments?item_id=${id}`);
+  const resolve = await fetch(`${baseURL}/${appID}/comments?item_id=${id}`);
   const result = await resolve.json();
 
   if (!result.length) {
@@ -28,26 +28,17 @@ const postComment = async (username, comment, id) => {
   return result;
 };
 
-// const countComment = async (id) => {
-//   const comments = await getComment(id);
-//   if (!comments.length) {
-//     return 0;
-//   }
-//   return comments.length;
-// };
-
-const commentsCounter = (commentsNum) => {
-  if (!commentsNum.length) {
+const commentsCounter = (comments) => {
+  if (!comments.length) {
     return 0;
   }
 
-  return commentsNum.length;
+  return comments.length;
 };
 
 const countComment = async (id) => {
-  const commentsNum = await getComment(id);
-
-  return countComment(commentsNum);
+  const comments = await getComment(id);
+  return commentsCounter(comments);
 };
 
 const displayComments = async (id) => {
@@ -70,10 +61,17 @@ const addComment = async (e, form, id) => {
   e.preventDefault();
   const { name, comment } = form.elements;
   const num = document.querySelector('.counter');
+  const ul = document.querySelector('.comment-container');
 
   await postComment(name.value, comment.value, id);
-  await displayComments(id);
-  num.textContent = await countComment(id);
+  const date = new Date();
+  ul.innerHTML += `
+    <li>
+      <span>${date.toLocaleDateString('en-US')}</span>
+      <span>${name.value}: </span>
+      <span>${comment.value}</span>
+    </li>`;
+  num.textContent = parseInt(num.textContent, 10) + 1;
   form.reset();
 };
 
