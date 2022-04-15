49f7213d6a73e6e4b96cdb17a1dc12d54fcef69a
Oybek Kayumov <oybek.kayumov@gmail.com>
2022-04-15 09:10:11 +0500

    update footer

--- modules/moviesCounter.js
+++ modules/moviesCounter.js
@@ -8,13 +8,6 @@ const counter = (items) => {
 };
 
 const countDisplay = (count) => {
-  // const navList = document.querySelector('.nav-list');
-  // const li = document.createElement('li');
-  // console.log(li);
-
-  // li.innerHTML = `<li class="nav-item">(${count}) MOVIES</li>`;
-  // navList.appendChild('li');
-
   const navList1 = document.querySelector('#count-movies');
   navList1.innerHTML = `<li class="nav-item">(${count}) MOVIES</li>`;
 };
